04cddb9f0941ab08f366c8aa2638df6a4e0bb5b3
ayushmishra950 <ayush950mishra@gmail.com>
2026-09-02 18:09:23 +0530

    upload cloudinary type error fixed

--- backend/cloudinary/uploadToCloudinary.ts
+++ backend/cloudinary/uploadToCloudinary.ts
@@ -1,12 +1,12 @@
 import cloudinary from "./cloudinaryConfig.ts";
-import type { UploadApiResponse, UploadApiErrorResponse } from "cloudinary";
+import type { UploadApiResponse, UploadApiErrorResponse, UploadApiOptions } from "cloudinary";
 
 const uploadToCloudinary = (fileBuffer:Buffer, mimetype:string, folder = "employees") => {
   return new Promise((resolve, reject) => {
     if (!fileBuffer) return reject(new Error("No file buffer provided"));
 
     // Determine resource type
-    let resource_type = "image"; // default
+   let resource_type: UploadApiOptions["resource_type"] = "image"; // default
     if (mimetype.startsWith("video/")) resource_type = "video";
     else if (mimetype.startsWith("audio/")) resource_type = "video"; // Cloudinary me audio ko video resource ke under upload karte hai
     else if (mimetype === "application/pdf") resource_type = "raw"; // PDF ya docs ke liye "raw"
